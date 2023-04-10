30c9c8fc5c5c864b56e946256458dc2bb6f16513
nagesh <npknagesh@gmail.com>
2023-04-10 13:29:20 +0530

    add login control and registration form

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
 from flask_login import login_user, logout_user, current_user, login_required
@@ -21,7 +22,7 @@ def index():
     ]
 
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -35,7 +36,10 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
