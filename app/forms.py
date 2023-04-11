6a202e932739c920e337a2fe1b8c3c396ec4d5d4
nagesh <npknagesh@gmail.com>
2023-04-11 14:42:52 +0530

    fixed bugs in edit template and edit form

--- app/forms.py
+++ app/forms.py
@@ -36,7 +36,7 @@ class EditProfileForm(FlaskForm):
     submit = SubmitField('Submit')
     
     def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(original_username)
+        super(EditProfileForm, self).__init__(*args, **kwargs)
         self.original_username = original_username
 
     def validate_username(self, username):
