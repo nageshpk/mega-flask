605685c9535d0ce56b9e83aaef48d264df3bc390
nagesh <npknagesh@gmail.com>
2023-04-12 21:01:40 +0530

    fixed bugs in reset password methods

--- app/email.py
+++ app/email.py
@@ -11,7 +11,7 @@ def send_mail(subject, sender, recipients, text_body, html_body):
 
 
 def send_password_reset_email(user):
-    token = user.generate_password_reset_token()
+    token = user.get_reset_password_token()
     send_mail('[Microblog] Reset your password',
               sender = app.config['ADMINS'][0],
               recipients=[user.email],
--- app/routes.py
+++ app/routes.py
@@ -158,7 +158,7 @@ def unfollow(username):
 
 
 @app.route('/reset_password_request', methods=['GET', 'POST'])
-def reset_password():
+def reset_password_request():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = ResetPasswordRequestForm()
