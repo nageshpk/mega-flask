56f364fe47d4873c0ff5a4e2edc26f601a4ab9b7
nagesh <npknagesh@gmail.com>
2023-04-12 17:15:05 +0530

    add pagination to user profile

--- app/routes.py
+++ app/routes.py
@@ -81,11 +81,13 @@ def register():
 def user(username):
     form = EmptyForm()
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test body #1'},
-        {'author': user, 'body': 'Test body #2'}
-    ]
-    return render_template('user.html', user = user, posts = posts, form=form)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False
+    )
+    next_url = url_for('user', username=current_user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=current_user.username, page=posts.prev_num) if posts.has_prev else None
+    return render_template('user.html', user = user, posts = posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 
 @app.before_request
