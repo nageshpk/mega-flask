250f6b9d9cd3e583274991170392c0f745463414
nagesh <npknagesh@gmail.com>
2023-04-12 20:51:27 +0530

    add reset password method

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User, Post
 from datetime import datetime
@@ -168,4 +168,20 @@ def reset_password():
             send_password_reset_email(user)
         flash("Check your email for instructions to reset your password")
         return redirect(url_for('login'))
-    return render_template('reset_password.html', title="Reset Password", form=form)
+    return render_template('reset_password_request.html', title="Reset Password", form=form)
+
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash("Your password has been reset.")
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
