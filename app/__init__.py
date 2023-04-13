085a4199e591c12fb97de7e70b630b8f44130eed
nagesh <npknagesh@gmail.com>
2023-04-13 14:10:29 +0530

    select all text needed to be translated

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from flask import request
 
 app = Flask(__name__)
@@ -18,6 +18,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please login to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
