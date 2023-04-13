b9e59cec5fb4a4a3b91ce8920e84c509df75c582
nagesh <npknagesh@gmail.com>
2023-04-13 22:59:43 +0530

    fixed bug

--- app/models.py
+++ app/models.py
@@ -1,12 +1,10 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
-from app import db, login
+from app import db, login, app
 from hashlib import md5
 from time import time
 import jwt
-from app import app
-
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
