f2b09b52526541ad905ca97c86a48a5c78adec89
nagesh <npknagesh@gmail.com>
2023-04-13 17:22:16 +0530

    fixed route for translations

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
@@ -6,7 +6,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User, Post
 from datetime import datetime
 from app.email import send_password_reset_email
-
+from flask_babel import get_locale
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -98,6 +98,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.locale = str(get_locale())
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
