8ee0f0053218085cdbbb75d27eba5fdddb219a1d
nagesh <npknagesh@gmail.com>
2023-05-24 11:09:45 +0530

    fixed bug of filter by

--- app/models.py
+++ app/models.py
@@ -199,7 +199,7 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
 
     @staticmethod
     def check_token(token):
-        user = User.query.fitler_by(token=token).first()
+        user = User.query.filter_by(token=token).first()
         if user is None or user.token_expiration < datetime.utcnow():
             return None
         return user
